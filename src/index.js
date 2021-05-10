139cbafcad97e75223e4b4a2aa5b7f16df3c0ab5
ooihuiying <huang.ooihuiying@gmail.com>
2021-05-10 17:24:36 +0800

    testing

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ require("dotenv").config({ path: __dirname + "/.env" });
 console.log("Welcome Page")
 console.log(process.env.PUBLIC_URL)
 ReactDOM.render(
-  <HashRouter>
+  <HashRouter basename={process.env.PUBLIC_URL}>
     <ErrorHandler>
       <Switch>
         <Route exact path="/" component={WelcomePage} />
