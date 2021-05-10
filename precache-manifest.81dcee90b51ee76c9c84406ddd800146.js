772abfef6d353f7fb766b64f1b5f179f58ef28dd
ooihuiying <huang.ooihuiying@gmail.com>
2021-05-10 15:25:02 +0800

    Updates

---
+++ precache-manifest.81dcee90b51ee76c9c84406ddd800146.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e51ba385c5479e76704ea0d7bea6bdc2",
+    "url": "/ooihuiying/index.html"
+  },
+  {
+    "revision": "175901a00e5ba270c32b",
+    "url": "/ooihuiying/static/js/2.a5dd852f.chunk.js"
+  },
+  {
+    "revision": "9327fa826028f9dc6dc36890a577d376",
+    "url": "/ooihuiying/static/js/2.a5dd852f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "196eef41ea2f34f945ab",
+    "url": "/ooihuiying/static/js/main.cc783fc6.chunk.js"
+  },
+  {
+    "revision": "b3417ac0268c0f8e599a",
+    "url": "/ooihuiying/static/js/runtime-main.7b49fc5c.js"
+  },
+  {
+    "revision": "2f37a260032bc340459531c70e657093",
+    "url": "/ooihuiying/static/media/ComputerScienceGirl.2f37a260.jpg"
+  },
+  {
+    "revision": "7ab22726927ee6473eed0667ed52c01a",
+    "url": "/ooihuiying/static/media/chinesepainting.7ab22726.jpg"
+  },
+  {
+    "revision": "9044d6dd226b7675e84280f156027cf9",
+    "url": "/ooihuiying/static/media/hongkong.9044d6dd.jpg"
+  },
+  {
+    "revision": "b6c30208ef65e86d655aa9aa71a8e8c9",
+    "url": "/ooihuiying/static/media/imagegirl.b6c30208.jpg"
+  },
+  {
+    "revision": "59fe56c906079a85712c6e797beb5bf7",
+    "url": "/ooihuiying/static/media/main.59fe56c9.svg"
+  },
+  {
+    "revision": "4f0626cd67c0af8d611fa88690eb1725",
+    "url": "/ooihuiying/static/media/mindful.4f0626cd.png"
+  },
+  {
+    "revision": "57b6b572e5050f41a80f02b53d67fdb9",
+    "url": "/ooihuiying/static/media/self.57b6b572.png"
+  },
+  {
+    "revision": "0da538fe9608a72e906448a803b627b6",
+    "url": "/ooihuiying/static/media/templeofheaven.0da538fe.jpg"
+  }
+]);
