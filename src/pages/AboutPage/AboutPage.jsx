d029607c7bcc51b1354fd480704be389f19686da
ooihuiying <huang.ooihuiying@gmail.com>
2023-06-23 15:59:15 +0800

    Update contents in AboutPage

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Background, AboutCard } from "./AboutPage.styles";
 import { Button, Icon, Image, Grid } from "semantic-ui-react"
 import PortalLayout from '../../layouts/PortalLayout';
-import imagegirl from '../../images/cheongsam.jpg';
+import imagegirl from '../../images/huiying.JPG';
 import { FaGlobeAsia } from 'react-icons/fa';
 const AboutPage = () => {
 
@@ -15,31 +15,31 @@ const AboutPage = () => {
           <AboutCard>
             <Grid >
               <Grid.Column width={3}>
-                <Image src={imagegirl} size='small' circular />
+                <Image src={imagegirl} size='small'/>
                 <h2><FaGlobeAsia color="maroon" />{" Singapore"}</h2>
                 <Button color='linkedin' onClick={(e) => {
                   e.preventDefault();
                   window.location.href = 'https://www.linkedin.com/in/hui-ying-ooi';
-                }}><Icon name='linkedin' />My Linkedin</Button>
+                }}><Icon name='linkedin' />Linkedin</Button>
                 <br></br>
                 <br></br>
                 <Button color='black' onClick={(e) => {
                   e.preventDefault();
                   window.location.href = 'https://github.com/ooihuiying';
-                }}><Icon name='github' />My GitHub</Button>
+                }}><Icon name='github' />GitHub</Button>
                 <br></br>
                 <br></br>
                 <Button color='purple' onClick={(e) => {
                   e.preventDefault();
-                  window.location.href = 'https://medium.com/@liverungrow';
-                }}><Icon name='medium' />My Medium</Button>
+                  window.location.href = 'https://liverungrow11.wordpress.com/';
+                }}><Icon name='wordpress' />Running Blog</Button>
               </Grid.Column>
               <Grid.Column width={12}>
                 <h1>About Me</h1>
                 <br />
-                <p> Hello 你好, I am Hui Ying. I'm a software engineer, earnest reader and writer with an overactive imagination. I am also an avid runner and I enjoy
-                  being in nature! </p>
-                <p>I have recently graduated from the National University of Singapore with a degree in Computer Science.</p>
+                <p> Hello 你好, I am Hui Ying.</p>
+                <p>I graduated from the National University of Singapore with a Bachelor degree in Computer Science in 2021.</p>
+                <p>Since then, I have been working as a Software engineer.</p>
                 <p>While I don’t have the best language skills, I do enjoy reading, writing and documenting things I have learned.
                 The intention of this website is to serve as a repository of articles, which i hope to write, in the areas of topics that
                 i am interested in. </p>
