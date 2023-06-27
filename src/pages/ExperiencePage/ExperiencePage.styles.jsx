b064b98cd8249a3b451eae6d724b6d0da48d04fb
ooihuiying <huang.ooihuiying@gmail.com>
2023-06-27 10:50:04 +0800

    Update design

--- src/pages/ExperiencePage/ExperiencePage.styles.jsx
+++ src/pages/ExperiencePage/ExperiencePage.styles.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import skater from '../../images/skater_solo.jpg';
 
 export const style = {
   main: {
@@ -11,6 +12,12 @@ export const style = {
 export const ExperiencePageContainer = styled.div`
   display: flex;
   justify-content: flex-start;
+  background-color: white;
+  background-image: url('${skater}');
+  background-repeat: no-repeat;
+  background-position: bottom right;
+  background-size: 200px;
+  margin-right: 50px;
 `;
 
 export const TimelineContainer = styled.div`
